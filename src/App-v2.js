693ac0380575f4d75cd87943689e34e5b22e10a2
Imoesi Rasheed Izuagbe <rasheedjay13@gmail.com>
2025-07-15 13:05:58 +0100

    Update

--- src/App-v2.js
+++ src/App-v2.js
@@ -1,6 +1,52 @@
 import { useEffect, useState } from "react";
 import StarRating from "./StarRating";
 
+/*const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];*/
 
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
@@ -11,16 +57,11 @@ const average = (arr) =>
 export default function App() {
     const [query, setQuery] = useState("");
     const [movies, setMovies] = useState([]);
-    
+    const [watched, setWatched] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState("");
     const [selectedId, setSelectedId] = useState(null);
     
-    const [watched, setWatched] = useState(function(){
-      const storedValue = localStorage.getItem("watched");
-      return JSON.parse(storedValue) || [];
-    });
-
    function handleSelectMovie(id) {
     setSelectedId((selectedId) => (id === selectedId ? null : id));
   
@@ -30,24 +71,14 @@ export default function App() {
   }
    
   function handleAddWatched(movie) {
-    setWatched((watched) => [...watched, movie]);
-
-    //localStorage.setItem('watched', JSON.stringify( [...watched, movie]));
+    setWatched(watched => [...watched, movie]);
   }
-
-
   function handleDeleteWatched(id) {
-    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id ));
+    setWatched(watched => watched.filter(movie => movie.imdbID !== id ));
   }
    
-  useEffect(function() {
-      localStorage.setItem("watched", JSON.stringify(watched));
-  }, 
-  [watched]);
 
-
-    useEffect(
-      function() {   
+    useEffect(function() {   
       const controller = new AbortController();
     async function fetchMovies() {
       try {
@@ -199,6 +230,30 @@ function Box({children}){
         </div>
   );
 }
+/*
+function WatchedBox() {
+  const [watched, setWatched] = useState(tempWatchedData);
+  const [isOpen2, setIsOpen2] = useState(true);
+
+  return (
+   
+        <div className="box">
+          <button
+            className="btn-toggle"
+            onClick={() => setIsOpen2((open) => !open)}
+          >
+            {isOpen2 ? "–" : "+"}
+          </button>
+          {isOpen2 && (
+            <>
+             
+            
+            </>
+          )}
+        </div>
+  );
+}
+*/
 
 function MovieList({movies, onSelectMovie}) {
 
@@ -245,14 +300,12 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
     Plot: plot, 
     Released: released,
     Actors: actors, 
+    Directors: directors, 
     Genre: genre 
   } = movie;
-  
-  const isTop = imdbRating>8;
-  console.log(isTop);
-  
+  console.log(movie);
 
-  function handleAdd() {
+  function handleAdd( onAddWatched ) {
     const newWatchedMovie = {
       imdbID: selectedId,
       title,
@@ -262,7 +315,7 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
       imdbRating: Number(imdbRating),
     }
     onAddWatched(newWatchedMovie);
-    //onCloseMovie();
+    onCloseMovie();
   }
 
     useEffect(function() {
@@ -333,10 +386,11 @@ useEffect(function() {
            
            </>
           ) : (<p>You rated this movie {watchedUserRating} 🌟</p>)}
-  
+          
        </div>
         <p>Plot by{plot}</p>
         <p>Starring {actors}</p>
+        <p>Directed by {directors}</p>
       </section>
       </>
       }
